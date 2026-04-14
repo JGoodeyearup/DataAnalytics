a1b7a0ff32a66e7217427042f56cba90d417caab
JGoodeyearup <jgoode@my.yearupunited.org>
2026-04-13 18:26:39 -0700

    Added queries of actor and film tables

--- week-02/Ex1B_First_Query.sql
+++ week-02/Ex1B_First_Query.sql
@@ -1,3 +1,8 @@
 /*
 I believe sakila is used to organize movie data such as actors' names, filming locations, and possible recording dates. 
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retreived 200 records
+
+SELECT * FROM film; -- Retreived 1000 records
