b0e85692e803590d7f131aac80c22796d0ff1491
JGoodeyearup <jgoode@my.yearupunited.org>
2026-05-05 18:46:23 -0700

    Files Updated

--- week-05/assignment/Ex1A_hello.py
+++ week-05/assignment/Ex1A_hello.py
@@ -1,1 +1,22 @@
 print('Hello World')
+
+message = 'Hello World'
+print(message)
+
+# hello world is being printed twice because one is a string and the other as a variable. 
+
+
+#displaying dollars and cents
+dollars = 3
+cents = 0.50
+
+print(dollars + cents)
+# I noticed that the output says 3.5 instead of 3.50 because I believe 
+# it's reading the numbers as an intergers and not a float.
+
+cents = cents + 0.25
+print(dollars + cents)
+
+d_str = '3 dollars '
+c_str = '50 cents'
+print(d_str + c_str)
