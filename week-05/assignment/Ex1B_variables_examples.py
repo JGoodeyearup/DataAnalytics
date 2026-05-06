f087f58f904731eddb36c329335170ce52404664
JGoodeyearup <jgoode@my.yearupunited.org>
2026-05-05 20:00:53 -0700

    Files Added

---
+++ week-05/assignment/Ex1B_variables_examples.py
@@ -0,0 +1,24 @@
+Ex1B_variables_examples.py
+
+customer_id = 2654
+customer_name = 'Craig' # I chose this name because it was the first to pop up in my head
+customer_gender = 'Non-binary' # I wanted to be inclusive and chose this
+customer_birth_date = '2000-12-23'# I was born the same year
+driver_license_number = 'D3124756'
+auto_policy_number = 'AP-243995'
+
+
+nick_name = 'Jay'
+city = 'Oakland'
+state = 'California'
+
+# Names that can't be used as variable names: 
+# False def if raise none del import return true elif in try and else is while as 
+#exceptlambda with assert finally nonlocalyeild break for not class form  of
+# continuegolbalpass
+
+# False = represents a boolean value of false oftenly used in conditions, comparisons, and logical operations.
+# IF = is a reserved keyword used to start a conditional statement that executes a block of code if a specified condition is true.
+# Import = is a reserved keyword used to include external modules or libraries in a Python program, allowing you to use their functions and features.
+# Finally = is a reserved keyword used in exception handling to define a block of code that will be executed regardless of whether an exception was raised or not.
+# Elif = is a reserved keyword used in conditional statements as a shorthand for "else if," allowing you to check multiple conditions sequentially.
