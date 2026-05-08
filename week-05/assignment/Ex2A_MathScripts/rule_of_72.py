6f50fe6c66d9bf5bb69c1f4696d6fcef310f6b75
JGoodeyearup <jgoode@my.yearupunited.org>
2026-05-07 17:48:47 -0700

    Files Added

---
+++ week-05/assignment/Ex2A_MathScripts/rule_of_72.py
@@ -0,0 +1,5 @@
+savings = 2000
+interest_rate = 0.6
+years = 72 / (interest_rate)
+
+print(f"Your saving is ${savings}. At a {format(interest_rate, '.0%')} interest rate, your savings will BE worth ${format(savings * 2, '.2f')} in {years} years.")
