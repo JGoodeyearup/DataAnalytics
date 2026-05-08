597750d44395504ed56138aa0f211b3ddb0adde3
JGoodeyearup <jgoode@my.yearupunited.org>
2026-05-08 16:23:59 -0700

    Files Added

---
+++ week-05/assignment/Ex3B_SampleCollections/movie_list.py
@@ -0,0 +1,9 @@
+movies = [" The Batman", "Paranormal Activity", "Saw", "Scary Movie", "Bring Her Back"]
+print(len(movies))
+print(f"This is a list of some of my favorite movies: {movies}")
+print(sorted(movies))
+print(movies)
+movies.sort()
+print(movies)
+movies.append("9")
+print(f"This is an updated version of my movie list: {movies}")
