6c3de462cd118b49ff9eb0d2c259c9f19a4f75e8
JGoodeyearup <jgoode@my.yearupunited.org>
2026-05-08 15:58:25 -0700

    Files Added

---
+++ week-05/assignment/Ex3A_string_cleaning.py
@@ -0,0 +1,9 @@
+name_1 = "Priya Sharma"
+name_2 = "bob NYGUYEN"
+name_3 = "LaTonya Williams"
+salary_1 = "$82,500"
+salary_2 = "74,000"
+
+print(name_1.lower(), name_2.lower(), name_3.lower())
+print(name_1.title(), name_2.title(), name_3.title())
+print(salary_1.replace('$', ''), salary_2.replace('$', ''))
