4b08d9dfbe829a1ffd3840ffe668da9136933ad8
JGoodeyearup <jgoode@my.yearupunited.org>
2026-05-07 18:28:33 -0700

    Files Updated

--- week-05/assignment/Ex2A_MathScripts/area_of_rectangle.py
+++ week-05/assignment/Ex2A_MathScripts/area_of_rectangle.py
@@ -2,3 +2,6 @@ side_A = float(input("Enter the length of side A: "))
 side_B = float(input("Enter the length of side B: "))
 area = side_A * side_B
 print(f"The area of the rectangle is: {area}")
+
+#After playing around with the input function, I found that if the user puts a word instead of a number 
+# it will cause an error. 
--- week-05/assignment/Ex2A_tips.py
+++ week-05/assignment/Ex2A_tips.py
@@ -4,7 +4,7 @@ tax = 6.54
 tip = 12.00
 # Calculate Total Cost
 total_due = food_cost + tax + tip
-print("Your total amount is: $", str(total_due)) # The reason I used the str() function is because I want to use the interger as a string
-print("Food Cost is $" + str(food_cost) + " and Tax is $" + str(tax))
-print("Your tip is $" + format(tip, ".2f"))
-print("Your total amount due is: $" + str(total_due))
+print(f"Your total amount is: ${format(total_due, '00.2f')}") # The reason I used the str() function is because I want to use the interger as a string
+print(f"Food Cost is ${format(food_cost, '00.2f')} and Tax is ${format(tax, '00.2f')}")
+print(f"Your tip is ${format(tip, '00.2f')}")
+print(f"Your total amount due is: ${format(total_due, '00.2f')}")
