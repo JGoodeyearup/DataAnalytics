b5a7690b27dcb105b523f32c2f31d9771c1cb3c6
JGoodeyearup <jgoode@my.yearupunited.org>
2026-05-08 21:37:12 -0700

    Files Updated

--- week-05/assignment/Ex4B_MoreIfScripts/dept_converter_v2.py
+++ week-05/assignment/Ex4B_MoreIfScripts/dept_converter_v2.py
@@ -15,5 +15,11 @@ match dept_code:
     case _:
         print(f"Please enter a valid department code")
 
-# I feel it would be more efficient to use the match case statement because it's easier to read and more straight to the point.
-# The if statement is easier to start with for me because I am more familiar with it but I think the more I use match case the more comfortable I'll be with it. 
+#  I feel it would be more efficient to use the match 
+# case statement because it's easier to read and more 
+# straight to the point.
+
+#  The if statement is easier to start with 
+# for me because I am more familiar with 
+# it but I think the more I use match case 
+# the more comfortable I'll be with it. 
