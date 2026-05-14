171c3e084b8ed970305d306c5a9f3955b26bb267
JGoodeyearup <jgoode@my.yearupunited.org>
2026-05-14 12:07:20 -0700

    Files Updated

---
+++ week-06/Work/Ex2B_Functions/math_and_stats.py
@@ -0,0 +1,11 @@
+import math
+import random
+import statistics
+
+vals_1_100 = range(1,100)
+vals_sample = random.sample(vals_1_100, 75)
+vals_choices = random.choices(vals_1_100, k = 200)
+radius = random.randint(3,10)
+pi = math.pi
+
+print(math.fsum(vals_1_100))
